7db05071ba2bd3163e2f4f0cb2546e82b3558980
magicbart <magicbart@gmail.com>
2023-10-29 17:03:00 +0100

    Feat: Init Vue3 + layout

---
+++ src/components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-navigation-drawer v-model="drawer" location="bottom" temporary>
+    <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+    <v-divider></v-divider>
+    <v-list-item link title="List Item 1"></v-list-item>
+    <v-list-item link title="List Item 2"></v-list-item>
+    <v-list-item link title="List Item 3"></v-list-item>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+</script>
---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar>
+      <v-app-bar-nav-icon :disabled=false variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" location="left">
+      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item link title="List Item 1"></v-list-item>
+      <v-list-item link title="List Item 2"></v-list-item>
+      <v-list-item link title="List Item 3"></v-list-item>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      Main Content
+    </v-main>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    group: null,
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+}
+</script>
