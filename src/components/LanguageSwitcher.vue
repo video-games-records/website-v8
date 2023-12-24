00e67d573ebb5ac1e90b1db531cbd0da8a6198f7
magicbart <magicbart@gmail.com>
2023-12-24 16:37:34 +0100

    Add language switcher

---
+++ src/components/LanguageSwitcher.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="lang-switcher">
+    <li>
+      <button @click="setLanguage('en')" class="button-as-link" :disabled="(getLanguage === 'en')">English</button>
+    </li>
+    <li>
+      <button @click="setLanguage('fr')" class="button-as-link" :disabled="(getLanguage === 'fr')">français</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'language-switcher',
+  computed: {
+    getLanguage() {
+      return localStorage.lang;
+    },
+  },
+  methods: {
+    setLanguage(lang) {
+      let name = this.$router.currentRoute.value.name;
+      let params = this.$router.currentRoute.value.params;
+      params['lang'] = lang;
+      this.$router.push({name: name, params: params});
+      console.log(this.$router);
+    },
+  },
+};
+</script>
--- src/components/security/AppBar.vue
+++ src/components/security/AppBar.vue
@@ -1,4 +1,5 @@
 <template>
+  <language-switcher />
   <account v-if="this.isAuthenticated" />
   <login v-if="!this.isAuthenticated" />
   <logout v-if="this.isAuthenticated" />
@@ -9,9 +10,10 @@ import Login from "@/components/security/btn/Login.vue";
 import Logout from "@/components/security/btn/Logout.vue";
 import Account from "@/components/security/btn/Account.vue";
 import Security from "@/mixins/Security.vue";
+import LanguageSwitcher from "@/components/LanguageSwitcher.vue";
 
 export default {
-  components: {Login, Logout, Account},
+  components: {LanguageSwitcher, Login, Logout, Account},
   mixins: [Security],
 }
 </script>
