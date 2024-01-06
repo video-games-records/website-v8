0c3f608a85eded1138ff0fb83c48dd4e9a197351
magicbart <magicbart@gmail.com>
2024-01-06 07:30:29 +0100

    Add video proof form

--- src/components/vgr/playerChart/proof/VideoForm.vue
+++ src/components/vgr/playerChart/proof/VideoForm.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <div v-if="showForm" class="container">
-      <form class="vgr-form" @submit="submit" @submit.prevent>
-        <div>
-          <label for="video-url">
-            URL
-            <span class="label-help">Exemple : https://youtu.be/iNSuKo8C5uk</span>
-          </label>
-          <input type="text" id="video-url" v-model="url" required="required">
-        </div>
-        <div>
-          <input type="submit" :value="$t('tag.submit')"/>
-        </div>
-      </form>
+      <v-form v-model="isValid" validate-on="blur" @submit.prevent @submit="submit()">
+        <v-sheet class="d-flex">
+          <v-text-field
+              density="compact"
+              label="URL"
+              placeholder="https://youtu.be/iNSuKo8C5uk"
+              v-model="url"
+              required="required"
+              :rules="[rules.required]" />
+          <v-btn type="submit">{{ $t('tag.submit')}}</v-btn>
+        </v-sheet>
+      </v-form>
     </div>
   </div>
 </template>
@@ -21,9 +21,10 @@
 
 import App from "@/mixins/App.vue";
 import {useFlashMessageStore} from "@/store/base/flashMessage";
+import Rules from "@/mixins/Rules.vue";
 
 export default {
-  mixins:[App],
+  mixins:[App, Rules],
   name: 'PlayerChartVideoForm',
   components: {},
   props: {
@@ -34,16 +35,13 @@ export default {
   },
   data() {
     return {
+      isValid: null,
       url: '',
-      isLoading: false,
       showForm: true,
     };
   },
   methods: {
     submit() {
-      this.loading = true;
-      this.success = null;
-
       const requestData = {
         method: 'post',
         url: "api/player-charts/" + this.playerChart.id +"/send-video",
@@ -54,15 +52,13 @@ export default {
 
       this.axios(requestData)
           .then(response => {
-            this.loading = false;
             this.showForm = false;
             this.hasSuccess = true;
-            this.updatePlayerChart()
+            this.updatePlayerChart();
+            useFlashMessageStore().confirm(this.$t('proof.form.success'));
           })
           .catch(error => {
-            this.loading = false;
-            this.hasError = true;
-            useFlashMessageStore().confirm(this.$t('proof.form.success'));
+            useFlashMessageStore().error('ERROR');
           });
     },
   },
