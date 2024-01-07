4ec0da06b87f5f2a7768bdc931524324bbb43a21
magicbart <magicbart@gmail.com>
2024-01-07 16:15:05 +0100

    Add team avatar prop size

--- src/components/vgr/team/Avatar.vue
+++ src/components/vgr/team/Avatar.vue
@@ -1,13 +1,23 @@
 <template>
   <div v-if="team.id" class="avatar">
-    <v-avatar :image="getAvatarUrl"></v-avatar>
+    <v-avatar :size="size" :image="getAvatarUrl"></v-avatar>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Avatar',
-  props: ['team'],
+  props: {
+    'team': {
+      type: Object,
+      require: true,
+    },
+    'size': {
+      type: String,
+      default: 'default',
+      require: false,
+    },
+  },
   computed: {
     getAvatarUrl() {
       return import.meta.env.VITE_ROOT_API + '/teams/' + this.team.id + '/avatar';
