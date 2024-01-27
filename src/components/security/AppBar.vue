5f147c8ec9e14a541030934dc37917a5f3a163dc
magicbart <magicbart@gmail.com>
2024-01-27 17:20:41 +0100

    Fix scroll

--- src/components/security/AppBar.vue
+++ src/components/security/AppBar.vue
@@ -2,20 +2,24 @@
   <proofs v-if="this.isAuthenticated" />
   <lost-position-box v-if="this.isAuthenticated" class="ma-3" />
   <message-box v-if="this.isAuthenticated" class="ma-3" />
+  <language-switcher />
   <account v-if="this.isAuthenticated" />
+  <login v-if="!this.isAuthenticated" />
   <logout v-if="this.isAuthenticated" />
 </template>
 
 <script>
+import Login from "@/components/security/btn/Login.vue";
 import Logout from "@/components/security/btn/Logout.vue";
 import Account from "@/components/security/btn/Account.vue";
 import Security from "@/mixins/Security.vue";
+import LanguageSwitcher from "@/components/LanguageSwitcher.vue";
 import MessageBox from "@/components/message/Box.vue";
 import LostPositionBox from "@/components/vgr/lost-position/Box.vue";
 import Proofs from "@/components/security/btn/Proofs.vue";
 
 export default {
-  components: {Proofs, LostPositionBox, MessageBox, Logout, Account},
+  components: {Proofs, LostPositionBox, MessageBox, LanguageSwitcher, Login, Logout, Account},
   mixins: [Security],
 }
 </script>
