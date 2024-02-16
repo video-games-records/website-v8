d11935dd94032cf7f515b81407812b5a095fb948
magicbart <magicbart@gmail.com>
2024-02-16 22:00:12 +0100

    Add avatar upload

--- src/views/account/Avatar.vue
+++ src/views/account/Avatar.vue
@@ -1,66 +1,56 @@
 <template>
   <v-sheet>
-    <h1 class="screen-reader-text">{{ $t('title') }}</h1>
-    <div>
-      <label for="fileInput">{{ $t('avatar.choose') }}</label>
-      <image-uploader
-          :debug="1"
-          :maxWidth="100"
-          :maxHeight="100"
-          :quality="0.9"
-          :autoRotate=false
-          outputFormat="verbose"
-          :preview=true
-          :className="['fileinput', { 'fileinput--loaded' : hasImage }]"
-          :capture="false"
-          accept="image/png,image/jpeg"
-          @input="setImage"
-          @onUpload="startImageResize"
-          @onComplete="endImageResize"
-      ></image-uploader>
-    </div>
-
-    <div>
-      <button :disabled="!hasImage" v-on:click="submitFile()">{{ $t('avatar.submit') }}</button>
-    </div>
-
-    <div v-if="hasMessage" role="alert">
-      {{ message }}
-    </div>
+    <h1 class="screen-reader-text">{{ $t('avatar.choose') }}</h1>
+    <v-row class="ma-2">
+      <v-col cols="12">
+        <v-file-input
+            v-model="files"
+            accept="image/png, image/jpeg, image/bmp"
+            placeholder="pick an image"
+            prepend-icon="mdi-camera"
+            :label="$t('avatar.choose')"
+            @update:modelValue="onChange()"
+        ></v-file-input>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title class="bg-primary">resized image</v-card-title>
+          <v-card-item>
+            <img alt="" :src="resizedImg" />
+          </v-card-item>
+          <v-card-actions>
+            <v-btn :disabled="resizedImg == ''" v-on:click="submitFile()">{{ $t('avatar.submit') }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-sheet>
 </template>
-
 <script>
-import ImageUploader from 'vue-image-upload-resize';
+
+import resizeImage from '@/plugins/image-resize.js';
 
 export default {
   name: 'AccountAvatar',
-  components: {
-    ImageUploader
-  },
+
   data() {
     return {
-      file: {},
-      apiResult: null,
-      message: null,
-      hasImage: false,
+      files: [],
+      resizedImg: '',
     };
   },
-  computed: {
-    hasMessage() {
-      return this.message != null;
-    },
-  },
+
   methods: {
-    startImageResize() {
-
-    },
-    endImageResize() {
-
-    },
-    setImage(file) {
-      this.hasImage = true;
-      this.file = file;
+    onChange() {
+      let file = this.files[0];
+      // start: preview resized
+      resizeImage({ file: file, maxSize: 100})
+        .then((resizedimage) => {
+          this.resizedImg = URL.createObjectURL(resizedimage);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     },
     submitFile() {
       /*UserApi.updloadAvatar(this.file.dataUrl).then(response => {
