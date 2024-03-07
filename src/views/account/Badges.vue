226cdef2aef063c6080b3572a8b633fa172a74a2
magicbart <magicbart@gmail.com>
2024-03-07 19:40:25 +0100

    Add order player badge

--- src/views/account/Badges.vue
+++ src/views/account/Badges.vue
@@ -1,10 +1,17 @@
 <template>
   <v-sheet class="ma-3">
-    <v-sheet class="d-flex flex-wrap">
-      <div v-for="item in badges" :data-position="item.position" :key="item['@id']" class="ma-1">
-        <badge v-bind:badge="item.badge" v-bind:createdAt="item.createdAt"></badge>
-      </div>
-    </v-sheet>
+    <v-row>
+      <v-col cols="12" class="d-flex flex-wrap">
+        <draggable v-model="badges" item-key="id">
+          <template #item="{element}">
+            <badge v-bind:badge="element.badge" v-bind:createdAt="element.createdAt" class="ma-1"></badge>
+          </template>
+        </draggable>
+      </v-col>
+      <v-col cols="12">
+        <v-btn @click="order">{{ $t('badge.order.button') }}</v-btn>
+      </v-col>
+    </v-row>
   </v-sheet>
 </template>
 
@@ -13,13 +20,17 @@ import PlayerBadgeMaster from "@/components/vgr/player/badge/Master";
 import Security from "@/mixins/Security.vue";
 import Badge from "@/components/vgr/badge/Badge.vue";
 import Date from "@/mixins/Date.vue";
+import draggable from 'vuedraggable';
+import axios from "axios";
+import {useFlashMessageStore} from "@/store/base/flashMessage";
 
 export default {
   mixins: [Security, Date],
   name: 'AccountBadges',
   components: {
     Badge,
-    PlayerBadgeMaster
+    PlayerBadgeMaster,
+    draggable
   },
   data() {
     return {
@@ -37,6 +48,23 @@ export default {
           .then(response => {
             this.badges = response.data['hydra:member'];
           })
+    },
+    order() {
+      let promises = [];
+      let i = 1;
+      let api = this.axios;
+      this.badges.forEach(function(element) {
+        if (element.mbOrder !== i) {
+          element.mbOrder = i;
+          promises.push(api.put('/api/player_badges/' + element.id, element));
+        }
+        i++;
+      });
+      axios.all(promises)
+          .then(() => {
+            this.load();
+            useFlashMessageStore().confirm(this.$t('badge.order.confirm'));
+          });
     },
   }
 };
