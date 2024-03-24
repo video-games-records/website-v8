246be4b6f61b19c19299093976f297325386cb1b
magicbart <magicbart@gmail.com>
2024-03-24 21:30:33 +0100

    Add register & reset password

--- src/components/security/btn/Login.vue
+++ src/components/security/btn/Login.vue
@@ -8,29 +8,37 @@
       </template>
 
       <template v-slot:default="{ isActive }">
-        <v-card title="Login">
-          <v-form v-model="isValid" validate-on="blur" @submit.prevent @submit="submit()">
-            <v-text-field v-model="email" :label="$t('authentication.login.form.email')" :rules="[rules.required]"></v-text-field>
-            <v-text-field v-model="password" type="password" :label="$t('authentication.login.form.password')" :rules="[rules.required]"></v-text-field>
+        <v-card>
+          <v-card-title>Login</v-card-title>
+          <v-card-item>
+            <v-form v-model="isValid" validate-on="blur" @submit.prevent @submit="submit()">
+              <v-text-field v-model="email" :label="$t('authentication.login.form.email')"
+                            :rules="[rules.required]"></v-text-field>
+              <v-text-field v-model="password" type="password" :label="$t('authentication.login.form.password')"
+                            :rules="[rules.required]"></v-text-field>
 
-            <div v-if="error" class="d-flex justify-center text-red">
-              {{ $t('authentication.login.form.error') }}
+              <div v-if="error" class="d-flex justify-center text-red">
+                {{ $t('authentication.login.form.error') }}
+              </div>
+
+              <v-btn v-if="!this.loading" type="submit" block class="mt-2">{{ $t('authentication.login.form.submit') }}</v-btn>
+              <div v-if="this.loading" class="d-flex justify-center">
+                <v-progress-circular indeterminate model-value="20"></v-progress-circular>
+              </div>
+            </v-form>
+
+            <v-divider />
+
+            <div class="ma-2">
+              <v-row>
+                <v-col cols="12" class="d-flex justify-center"><router-link @click="isActive.value = false" :to="{ name: 'Register'}">{{ $t('authentication.register.title') }}</router-link> </v-col>
+                <v-col cols="12" class="d-flex justify-center"><router-link @click="isActive.value = false" :to="{ name: 'AuthReset'}">{{ $t('authentication.login.forgotten_password') }}</router-link></v-col>
+              </v-row>
             </div>
+          </v-card-item>
 
-            <v-btn v-if="!this.loading" type="submit" block class="mt-2">{{ $t('authentication.login.form.submit') }}</v-btn>
-            <div v-if="this.loading" class="d-flex justify-center">
-              <v-progress-circular indeterminate model-value="20"></v-progress-circular>
-            </div>
-          </v-form>
 
-          <v-divider />
 
-          <div class="ma-2">
-            <v-row>
-              <v-col cols="12" class="d-flex justify-center"><router-link @click="isActive.value = false" :to="{ name: 'Register'}">{{ $t('authentication.register.title') }}</router-link> </v-col>
-              <v-col cols="12" class="d-flex justify-center"><router-link @click="isActive.value = false" :to="{ name: 'AuthReset'}">{{ $t('authentication.login.forgotten_password') }}</router-link></v-col>
-            </v-row>
-          </div>
 
           <v-card-actions>
             <v-spacer></v-spacer>
