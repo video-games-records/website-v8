4d14cbeab47a828a5ef7baaedb121ae1546712ba
magicbart <magicbart@gmail.com>
2024-06-09 15:55:37 +0200

    scrollBehavior on top

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
     Sprite,
   },
   mounted() {
-    //setTimeout(() => this.scrollFix(this.$route.hash), 1);
+    setTimeout(() => this.scrollFix(this.$route.hash), 1);
   },
   created() {
     useTheme().global.name.value = useThemeStore().getName;
