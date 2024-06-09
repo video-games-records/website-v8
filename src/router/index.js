b517141dd74876dbc81e544b27051c69cbbae7da
magicbart <magicbart@gmail.com>
2024-06-09 15:55:37 +0200

    scrollBehavior on top

--- src/router/index.js
+++ src/router/index.js
@@ -171,6 +171,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { top: 0 }
+  }
 })
 
 router.beforeEach(async (to, from, next) => {
